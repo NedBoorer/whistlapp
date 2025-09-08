9f4a2df0f4d8ef3c02fe0e5c66d36cf104e7569e
Ned Boorer <nedboorer@outlook.com>
2025-09-08 16:38:16 +1000

    Initial Commit

---
+++ whistl/whistlApp.swift
@@ -0,0 +1,17 @@
+//
+//  whistlApp.swift
+//  whistl
+//
+//  Created by Ned Boorer on 8/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct whistlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
